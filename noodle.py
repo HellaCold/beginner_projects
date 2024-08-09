1bb954fa5b03cb28be9fd64c0d315738691691e6
HellaCold <ifredrick2020@gmail.com>
2024-08-09 18:41:34 -0500

    cleaned it up

--- noodle.py
+++ noodle.py
@@ -2,20 +2,15 @@ name = input("Whoof I mean Hey my name is Noodle whats your's? ")
 
 print("Nice to meet ya! " + name )
 
- = inpresponseut("Would you like to play a number guessing game with me? \nyes or no? ").strip().lower()
+response = input("Would you like to play a number guessing game with me? \nyes or no? ").strip().lower()
 
 
-while true:
+while "yes":
     if response == "yes":
         diff = input("THAT'S GREAT! \nok ok \nWould you like to be \n1.Easy \n2.Kinda Easy \n3.Hard \n? ")
-            if diff == easy:
-            elif == 1:
-                print("you really going to put 1 instead of typing out easy?")
-                
-            elif diff == med:
-            elif diff == 2:
-            elif diff == hard:
-            else diff ==3:
+        if diff == "1":
+            print("you really going to put 1 instead of typing out easy?")
+
 
     else:
         print("awwwww maybe next time.Have a good day " + name + "!")
