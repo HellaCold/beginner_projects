aaedf07f5a1346ab3fad833d7486e6dba425d1a0
HellaCold <ifredrick2020@gmail.com>
2024-08-16 18:09:01 -0500

    cleaned up some #2

--- noodle.py
+++ noodle.py
@@ -4,11 +4,9 @@ print("Nice to meet ya! " + name )
 
 response = input("Would you like to play a number guessing game with me? \nyes or no? ").strip().lower()
 
-
 if response == "yes":
     guess_1 = input("Awesome ill start off easy.\nI'm thinking of a number from 1-10.\What do you thin it is? ")
 
-
-    else:
-        print("awwwww maybe next time.Have a good day " + name + "!")
+else:
+    print("awwwww maybe next time.Have a good day " + name + "!")
         #OKAY i think i got it
